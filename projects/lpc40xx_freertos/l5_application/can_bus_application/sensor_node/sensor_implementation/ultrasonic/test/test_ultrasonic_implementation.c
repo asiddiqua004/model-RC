2c9782dde4c02e3ca072f087279dbbd9c17da65d
Danny Nuch <dannynuch@gmail.com>
2020-04-02 19:41:55 +0000

    Sensor node implementation

---
+++ projects/lpc40xx_freertos/l5_application/can_bus_application/sensor_node/sensor_implementation/ultrasonic/test/test_ultrasonic_implementation.c
@@ -0,0 +1,42 @@
+#include "unity.h"
+
+#include "Mockgpio2_interrupts.h"
+#include "Mocksys_time.h"
+#include "Mockultrasonic.h"
+
+#include "ultrasonic_implementation.h"
+
+void setUp(void) {}
+void tearDown(void) {}
+
+void test_ultrasonic_implementation__initialize(void) {
+  ultrasonic_s ultrasonic = {0};
+
+  ultrasonic__initialize_Expect(&ultrasonic, GPIO__PORT_0, 6U, GPIO__PORT_2, 0U);
+  ultrasonic__initialize_Expect(&ultrasonic, GPIO__PORT_0, 7U, GPIO__PORT_2, 1U);
+  ultrasonic__initialize_Expect(&ultrasonic, GPIO__PORT_0, 8U, GPIO__PORT_2, 2U);
+  ultrasonic__initialize_Expect(&ultrasonic, GPIO__PORT_0, 9U, GPIO__PORT_2, 4U);
+
+  gpio2_interrupts__attach_handler_Expect(0U, FALLING, NULL);
+  gpio2_interrupts__attach_handler_IgnoreArg_isr_handler();
+  gpio2_interrupts__attach_handler_Expect(1U, FALLING, NULL);
+  gpio2_interrupts__attach_handler_IgnoreArg_isr_handler();
+  gpio2_interrupts__attach_handler_Expect(2U, FALLING, NULL);
+  gpio2_interrupts__attach_handler_IgnoreArg_isr_handler();
+  gpio2_interrupts__attach_handler_Expect(4U, FALLING, NULL);
+  gpio2_interrupts__attach_handler_IgnoreArg_isr_handler();
+  gpio2_interrupts__initialize_Expect();
+
+  ultrasonic_implementation__initialize();
+}
+
+void test_ultrasonic_implementation__initiate_ultrasonics_range(void) {
+  ultrasonic_s ultrasonic = {0};
+
+  ultrasonic__initiate_range_Expect(&ultrasonic);
+  ultrasonic__initiate_range_Expect(&ultrasonic);
+  ultrasonic__initiate_range_Expect(&ultrasonic);
+  ultrasonic__initiate_range_Expect(&ultrasonic);
+
+  ultrasonic_implementation__initiate_ultrasonics_range();
+}
