ac31d2f17fd025796576ced5a7b6e8809ee82e56
dnuch <dannynuch@gmail.com>
2020-05-14 19:23:07 -0700

    add avg buffer module

--- projects/sensor/l5_application/can_bus_application/sensor_node/sensor_implementation/ultrasonic/ultrasonic_implementation.c
+++ projects/sensor/l5_application/can_bus_application/sensor_node/sensor_implementation/ultrasonic/ultrasonic_implementation.c
@@ -2,29 +2,39 @@
 
 #include <stdio.h>
 
+#include "avg_buffer.h"
 #include "gpio2_interrupts.h"
 #include "sys_time.h"
 #include "ultrasonic.h"
 
 static ultrasonic_s front_ultrasonic, back_ultrasonic, left_ultrasonic, right_ultrasonic;
 
+static avg_buffer_s front_avg_buffer, back_avg_buffer, left_avg_buffer, right_avg_buffer;
+#define AVG_BUFFER_CAPACITY 2U
+static uint8_t front_buffer[AVG_BUFFER_CAPACITY], back_buffer[AVG_BUFFER_CAPACITY], left_buffer[AVG_BUFFER_CAPACITY],
+    right_buffer[AVG_BUFFER_CAPACITY];
+
 static void front_ultrasonic_echo_falling_edge_interrupt_handler(void) {
   front_ultrasonic.time_of_flight_us = sys_time__get_uptime_us() - front_ultrasonic.last_trigger_time_us;
+  avg_buffer__insert_value(&front_avg_buffer, front_ultrasonic.time_of_flight_us / 148U);
   front_ultrasonic.start_new_reading = true;
 }
 
 static void back_ultrasonic_echo_falling_edge_interrupt_handler(void) {
   back_ultrasonic.time_of_flight_us = sys_time__get_uptime_us() - back_ultrasonic.last_trigger_time_us;
+  avg_buffer__insert_value(&back_avg_buffer, back_ultrasonic.time_of_flight_us / 148U);
   back_ultrasonic.start_new_reading = true;
 }
 
 static void left_ultrasonic_echo_falling_edge_interrupt_handler(void) {
   left_ultrasonic.time_of_flight_us = sys_time__get_uptime_us() - left_ultrasonic.last_trigger_time_us;
+  avg_buffer__insert_value(&left_avg_buffer, left_ultrasonic.time_of_flight_us / 148U);
   left_ultrasonic.start_new_reading = true;
 }
 
 static void right_ultrasonic_echo_falling_edge_interrupt_handler(void) {
   right_ultrasonic.time_of_flight_us = sys_time__get_uptime_us() - right_ultrasonic.last_trigger_time_us;
+  avg_buffer__insert_value(&right_avg_buffer, right_ultrasonic.time_of_flight_us / 148U);
   right_ultrasonic.start_new_reading = true;
 }
 
@@ -41,6 +51,12 @@ void ultrasonic_implementation__initialize(void) {
   gpio2_interrupts__attach_handler(2U, FALLING, left_ultrasonic_echo_falling_edge_interrupt_handler);
   gpio2_interrupts__attach_handler(4U, FALLING, right_ultrasonic_echo_falling_edge_interrupt_handler);
   gpio2_interrupts__initialize();
+
+  // Initialize averaging buffers
+  avg_buffer__initialize(&front_avg_buffer, front_buffer, AVG_BUFFER_CAPACITY);
+  avg_buffer__initialize(&back_avg_buffer, back_buffer, AVG_BUFFER_CAPACITY);
+  avg_buffer__initialize(&left_avg_buffer, left_buffer, AVG_BUFFER_CAPACITY);
+  avg_buffer__initialize(&right_avg_buffer, right_buffer, AVG_BUFFER_CAPACITY);
 }
 
 // Will block for (10 us * 4) = 40 us in total
@@ -51,20 +67,20 @@ void ultrasonic_implementation__initiate_ultrasonics_range(void) {
   ultrasonic__initiate_range(&right_ultrasonic);
 }
 
-uint16_t ultrasonic_implementation__get_front_ultrasonic_distance_in(void) {
-  return (front_ultrasonic.time_of_flight_us / 148U);
+uint8_t ultrasonic_implementation__get_front_ultrasonic_distance_in(void) {
+  return avg_buffer__get_value(&front_avg_buffer);
 }
 
-uint16_t ultrasonic_implementation__get_back_ultrasonic_distance_in(void) {
-  return (back_ultrasonic.time_of_flight_us / 148U);
+uint8_t ultrasonic_implementation__get_back_ultrasonic_distance_in(void) {
+  return avg_buffer__get_value(&back_avg_buffer);
 }
 
-uint16_t ultrasonic_implementation__get_left_ultrasonic_distance_in(void) {
-  return (left_ultrasonic.time_of_flight_us / 148U);
+uint8_t ultrasonic_implementation__get_left_ultrasonic_distance_in(void) {
+  return avg_buffer__get_value(&left_avg_buffer);
 }
 
-uint16_t ultrasonic_implementation__get_right_ultrasonic_distance_in(void) {
-  return (right_ultrasonic.time_of_flight_us / 148U);
+uint8_t ultrasonic_implementation__get_right_ultrasonic_distance_in(void) {
+  return avg_buffer__get_value(&right_avg_buffer);
 }
 
 void ultrasonic_implementation__print_ultrasonics_range(void) {
