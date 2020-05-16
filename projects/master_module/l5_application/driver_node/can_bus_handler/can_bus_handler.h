48ce9fdd0853790fcdd6f7a60afa372285e9b372
dnuch <dannynuch@gmail.com>
2020-05-15 18:49:39 -0700

    remove master module

--- projects/master_module/l5_application/driver_node/can_bus_handler/can_bus_handler.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-void can_handler__transmit_heartbeat_10hz(void);
-void can_handler__manage_mia_10hz(void);
-void can_handler__handle_all_incoming_messages_10Hz(void);
-void populate_signal_status_10Hz(void);
-void can_handler__transmit_messages_10hz(void);
-void can_handler__transmit_debug_msg_10hz(void);
--- projects/master_module/l5_application/driver_node/can_bus_initializer/can_bus_initializer.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <stdbool.h>
-void can_bus_initializer__init(void);
-void can_bus_initializer__turn_can_bus_on_if_off(void);
-bool can_bus_initializer__get_can_bus_init_status(void);
-bool can_bus_initializer__get_can_bus_off(void);
